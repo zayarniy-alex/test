cf49cb2feeb0446b379224c6c70f13395670c36f
Rene Kluwen <rene@kluwen.com>
2019-11-08 20:30:39 +0100

    - Fixing "Release" version of Domoticz x64. - updated packages.bat to do a full vcpkg install.

--- packages.bat
+++ packages.bat
@@ -1,4 +1,8 @@
 set VCPKG_DEFAULT_TRIPLET=x64-windows
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg
+git pull
+call bootstrap-vcpkg.bat
 vcpkg install zlib
 vcpkg install openssl
 vcpkg install pthreads
@@ -11,3 +15,4 @@ vcpkg install sqlite3
 vcpkg install curl
 vcpkg install lua
 vcpkg integrate project
+ 
